beff327ed4b815729ddc43f3d7990ee7c6de4ef9
atsuage333 <127850785+atsuage333@users.noreply.github.com>
2024-02-24 18:03:35 +0900

    dawdwa

--- main/doors/doors.lua
+++ main/doors/doors.lua
@@ -1,6 +1,31 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/ionlyusegithubformcmods/1-Line-Scripts/main/Mobile%20Friendly%20Orion')))()
 local Window = OrionLib:MakeWindow({Name = "AtsuageZHUB Doors🚪👁", HidePremium = false, SaveConfig = true, ConfigFolder = "Doors Summon"})
 
+local HomeTab = Window:MakeTab({
+	Name = "Home",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+HomeTab:AddToggle({
+	Name = "Anti AFK",
+	Default = false,
+	Callback = function(Value)
+		local vu = game:GetService("VirtualUser")
+game:GetService("Players").LocalPlayer.Idled:connect(function()
+vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+wait(1)
+vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+end)
+	end    
+})
+
+HomeTab:AddButton({
+	Name = "Destroy Gui",
+	Callback = function()
+	      OrionLib:Destroy()
+  	end    
+})
 
 local itemsTab = Window:MakeTab({
     Name = "Items",
